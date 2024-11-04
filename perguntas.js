3adc5bae8ae0035fa62378b96b308e67cc4ca871
Caue2k24 <00001105449051sp@al.educacao.sp.gov.br>
2024-11-04 11:38:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 7 + 3?' ,
+    '10!'
+)
+
+criaCartao(
+    'Técnologia e Robótica',
+    'Qual o comando que desfaz uma ação no VsCode?',
+    'Ctrl Z'
+)
+
+criaCartao(
+    'Empreendedorismo',
+    'Oque é empreendedorismo?',
+    'inciativa de implementar novos negócios ou mudanças em empresas já existentes, ger. com alterações que envolvem inovação e riscos.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como fala "Bom dia" em ingles?',
+    'God Day'
+)
